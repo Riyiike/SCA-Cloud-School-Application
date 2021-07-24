5e26b127fd553c95210b62c37172392e7c805353
Riyiike <odusanyaiyanuoluwa@gmail.com>
2021-07-24 23:41:19 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const participant = {
+    firstName: "Iyanuoluwa",
+    lastName: "Odusanya",
+    age: 25,
+    Application:"Cloud Engineering",
+    language:"javascript",
+    fullName : function() {
+        return this.firstName + " " + this.lastName;
+      }
+  };
